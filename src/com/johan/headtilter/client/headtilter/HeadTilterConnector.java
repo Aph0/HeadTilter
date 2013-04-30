8ee983468a37ad0b368288dcde63863e04388463
johan <johan@vaadin.com>
2013-04-30 17:00:52 +0300

    Added support for headClick at client side. Now only calibration needed

--- src/com/johan/headtilter/client/headtilter/HeadTilterConnector.java
+++ src/com/johan/headtilter/client/headtilter/HeadTilterConnector.java
@@ -1,8 +1,13 @@
 package com.johan.headtilter.client.headtilter;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.dom.client.Style.Visibility;
+import com.google.gwt.touch.client.Point;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Widget;
@@ -22,38 +27,149 @@ public class HeadTilterConnector extends AbstractExtensionConnector {
 	private String rotationStringBrowserSpecific;
 	private Element canvas;
 	private Element video;
+	private HeadTilterMode mode = HeadTilterMode.MOUSE_CURSOR;
+	private HeadClickTracker hcTracker;
+
+	// This guy decides when and where a click has occurred
+	private static class HeadClickTracker {
+		/**
+		 * Move head to the left, over 15 degree limit, then inside some time,
+		 * move back = click
+		 * 
+		 * @author johan
+		 * 
+		 */
+		private float angleSensitivity = 0.12f; // radians, TODO
+		private float coordinateSensitivity = 2f;
+
+		private boolean checkIfClickPatternFound() {
+			boolean containsOverLimit = false;
+			boolean containsUnderLimit = false;
+
+			for (Float angle : lastAngles) {
+				// 90 degrees ~= 1.57 radians
+				if (angle > 1.57 + angleSensitivity)
+					containsOverLimit = true;
+				if (angle < 1.57 - angleSensitivity)
+					containsUnderLimit = true;
+			}
+
+			return containsOverLimit && containsUnderLimit;
+		}
+		
+		private Point createAveragePointFrom(List<Point> pList) {
+			int lSize = pList.size();
+			double xTot = 0;
+			double yTot = 0;
+			for (Point p : lastHeadPositions) {
+				xTot += p.getX();
+				yTot += p.getY();
+			}
+			return new Point(xTot / lSize, yTot / lSize);
+		}
+
+		private Point estimateHeadClickPosition() {
+			int limit = 10; // TODO, create some var
+
+			if (lastHeadPositions.size() <= 0)
+				return null;
+
+			if (lastHeadPositions.size() >= limit) {
+				float xAverageVal = 0;
+				int occurrences = 0;
+				float yAverageVal = 0;
+				int examined = 0;
+
+
+
+				for (Point point : lastHeadPositions) {
+					if (examined >= limit || occurrences >= 4) break; // Only step back 'limit' amount of steps or that we have enough occurrences
+					
+					if (xAverageVal > 0 || (point.getX() <= xAverageVal / occurrences + coordinateSensitivity
+							&& point.getX() >= xAverageVal / occurrences
+									- coordinateSensitivity
+							&& point.getY() <= yAverageVal / occurrences
+									+ coordinateSensitivity
+							&& point.getY() >= yAverageVal / occurrences
+									- coordinateSensitivity)) {
+						xAverageVal += point.getX();
+						yAverageVal += point.getY();
+						occurrences++;
+					} else {
+						xAverageVal = (float) point.getX();
+						yAverageVal = (float) point.getY();
+						occurrences = 1;
+					}
+					examined++;
+				}
+				
+				if (occurrences >= 4) {
+					return new Point(xAverageVal / occurrences, yAverageVal / occurrences);
+				} else {
+					return createAveragePointFrom(lastHeadPositions.subList(0, limit));
+				}
+
+			} else {
+				return createAveragePointFrom(lastHeadPositions);
+
+			}
+		}
+
+		private LinkedList<Point> lastHeadPositions = new LinkedList<Point>();
+
+		private LinkedList<Float> lastAngles = new LinkedList<Float>();
+
+		/**
+		 * Returns a Point(x, y) if there was a click, otherwise null
+		 */
+		public Point isHeadClick(float x, float y, float angle) {
+			lastHeadPositions.addFirst(new Point(x, y));
+			lastAngles.addFirst(angle);
+			if (lastHeadPositions.size() > 20) {
+				lastHeadPositions.removeLast();
+				lastAngles.removeLast();
+			}
+			if (checkIfClickPatternFound()) {
+				Point clicked = estimateHeadClickPosition();
+				lastHeadPositions.clear();
+				lastAngles.clear();
+				return clicked;
+			}
+			return null;
+		}
+
+	}
 
 	public HeadTilterConnector() {
-
+		hcTracker = new HeadClickTracker();
 	}
 
 	@Override
 	protected void init() {
 		super.init();
-        rotationStringBrowserSpecific = "transform";
-        String prefix = getBrowserPrefix();
-        if (prefix != null) {
-            rotationStringBrowserSpecific = getBrowserPrefix()
-                    + "Transform";
-        }
+		rotationStringBrowserSpecific = "transform";
+		String prefix = getBrowserPrefix();
+		if (prefix != null) {
+			rotationStringBrowserSpecific = getBrowserPrefix() + "Transform";
+		}
 		declareHeadMoveEventMethod(this);
 		registerHeadTiltingListeners();
 	}
-	
-    private String getBrowserPrefix() {
-        String prefix = null;
-        final BrowserInfo bi = BrowserInfo.get();
-        if (bi.isWebkit()) {
-            prefix = "webkit";
-        } else if (bi.isIE()) {
-            prefix = "ms";
-        } else if (bi.isOpera()) {
-            prefix = "O";
-        } else if (bi.isFirefox()) {
-            prefix = "Moz";
-        }
-        return prefix;
-    }
+
+	private String getBrowserPrefix() {
+		String prefix = null;
+		final BrowserInfo bi = BrowserInfo.get();
+		if (bi.isWebkit()) {
+			prefix = "webkit";
+		} else if (bi.isIE()) {
+			prefix = "ms";
+		} else if (bi.isOpera()) {
+			prefix = "O";
+		} else if (bi.isFirefox()) {
+			prefix = "Moz";
+		}
+		return prefix;
+	}
 
 	@Override
 	public void onStateChanged(StateChangeEvent stateChangeEvent) {
@@ -68,44 +184,63 @@ public class HeadTilterConnector extends AbstractExtensionConnector {
 	}
 
 	private void registerHeadTiltingListeners() {
-		canvas = DOM.createElement("canvas");
-		canvas.setId("inputCanvas");
-		canvas.setAttribute("width", "320");
-		canvas.setAttribute("height", "240");
-		// canvas.getStyle().setDisplay(Display.NONE);
+		if (DOM.getElementById("inputCanvas") == null) {
+			canvas = DOM.createElement("canvas");
+			canvas.setId("inputCanvas");
+			canvas.setAttribute("width", "320");
+			canvas.setAttribute("height", "240");
+			// canvas.getStyle().setDisplay(Display.NONE);
+			Document.get().getBody().appendChild(canvas);
+		}
 
-		video = DOM.createElement("video");
-		video.setId("inputVideo");
-		video.setAttribute("autoplay", null);
-		video.setAttribute("loop", null);
-		video.getStyle().setWidth(1, Unit.PX);
-		video.getStyle().setHeight(1, Unit.PX);
-		video.getStyle().setVisibility(Visibility.HIDDEN);
+		if (DOM.getElementById("video") == null) {
 
-		Document.get().getBody().appendChild(canvas);
-		Document.get().getBody().appendChild(video);
+			video = DOM.createElement("video");
+			video.setId("inputVideo");
+			video.setAttribute("autoplay", null);
+			video.setAttribute("loop", null);
+			video.getStyle().setWidth(1, Unit.PX);
+			video.getStyle().setHeight(1, Unit.PX);
+			video.getStyle().setVisibility(Visibility.HIDDEN);
+			Document.get().getBody().appendChild(video);
+		}
 		initiateHeadTilter();
 	}
 
 	public void handleHeadMoveEvent(float x, float y, float angle,
 			float height, float width) {
-		if (extendedWidget != null) {
-			extendedWidget.getElement()
-            .getStyle()
-            .setProperty(rotationStringBrowserSpecific,
-                    "rotate(" + (-angle * (180 / 3.1415) + 90) + "deg)");
+		boolean clickedWithHead = false;
+		if (mode == HeadTilterMode.TILT_WIDGETS) {
+			if (extendedWidget != null) {
+				extendedWidget
+						.getElement()
+						.getStyle()
+						.setProperty(
+								rotationStringBrowserSpecific,
+								"rotate(" + (-angle * (180 / 3.1415) + 90)
+										+ "deg)");
+			}
+		} else {
+
+			Point clickPt = hcTracker.isHeadClick(x, y, angle);
+			if (clickPt != null) {
+				System.out.println("Headclick at: " + clickPt);
+			}
 		}
 		System.out.println("moved x: " + x + " y: " + y + " angle: " + angle
 				+ "   height: " + height + " width: " + width);
+		if (clickedWithHead)
+			System.out.println("HEADLICK!");
 	}
 
-	public static native void declareHeadMoveEventMethod(HeadTilterConnector headTilterConnector) /*-{
-													var headTilterConnector = headTilterConnector;
-													
-													$wnd.handleHeadMoveEvent = function(x, y, angle, height, width) {
+	public static native void declareHeadMoveEventMethod(
+			HeadTilterConnector headTilterConnector) /*-{
+														var headTilterConnector = headTilterConnector;
+														
+														$wnd.handleHeadMoveEvent = function(x, y, angle, height, width) {
 														$entry(headTilterConnector.@com.johan.headtilter.client.headtilter.HeadTilterConnector::handleHeadMoveEvent(FFFFF)(x, y, angle, height, width));
-													}
-													}-*/;
+														}
+														}-*/;
 
 	private native void initiateHeadTilter() /*-{
 												var videoInput = $wnd.document.getElementById('inputVideo');
@@ -121,10 +256,10 @@ public class HeadTilterConnector extends AbstractExtensionConnector {
 												}-*/;
 
 	private native void clearUp() /*-{
-			$wnd.htracker.stop();
-			// TODO: remove facetrackingEvent also
-	}-*/;
-	
+									$wnd.htracker.stop();
+									// TODO: remove facetrackingEvent also
+									}-*/;
+
 	@Override
 	public void onUnregister() {
 		Document.get().getBody().removeChild(canvas);
---
+++ src/com/johan/headtilter/client/headtilter/HeadTilterMode.java
@@ -0,0 +1,5 @@
+package com.johan.headtilter.client.headtilter;
+
+public enum HeadTilterMode {
+	TILT_WIDGETS, MOUSE_CURSOR
+}
